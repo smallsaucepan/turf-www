6d2e4654a3dc5e32c48766012eb90fffa8f460ac
James Beard <james@smallsaucepan.com>
2024-04-11 19:35:13 +1000

    Github pages config to hopefully deploy a test website.

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -14,15 +14,16 @@ const config = {
   favicon: "img/favicon.ico",
 
   // Set the production url of your site here
-  url: "https://turfjs.org",
+  url: "https://www.turfjs.smallsaucepan.com",
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
   baseUrl: "/",
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
-  organizationName: "Turfjs", // Usually your GitHub org/user name.
-  projectName: "turf", // Usually your repo name.
+  organizationName: "smallsaucepan", // Usually your GitHub org/user name.
+  projectName: "turf-www", // Usually your repo name.
+  trailingSlash: false,
 
   onBrokenLinks: "throw",
   onBrokenMarkdownLinks: "warn",
